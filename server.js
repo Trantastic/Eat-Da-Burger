10963600f27eb6ac9845fa5c424c144e20013b2f
Yvette <Yvette@macs-MacBook-Air.local>
2018-03-01 05:50:28 -0800

    Created setup for server and folders

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burger_controller.js");
+
+app.use("/", routes);
+
+app.listen(port);
