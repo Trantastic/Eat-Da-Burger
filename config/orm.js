7684335f89c4223a73d97b8236c080018642227b
Yvette <Yvette@macs-MacBook-Air.local>
2018-03-02 09:10:49 -0800

    added helped function to create query

--- config/orm.js
+++ config/orm.js
@@ -1,10 +1,36 @@
-var connection = require("connection.js");
+var connection = require("./connection.js");
+
+// Helper function to assemble ? for mysql
+function printQuestionMarks(num){
+	var arr = [];
+
+	for(var i = 0; i < num; i++){
+		arr.push("?");
+	}
+	return arr.toString();
+}
+// Helper function to convert object keys and values for mysql
+function obToSql(ob){
+	var arr = [];
+
+	for(var key in ob){
+		var value = ob[key];
+
+		if(Object.hasOwnProperty.call(ob, key)){
+			if(typeof value === "string" && value.indexOf(" ") >= 0){
+				value = "'" + value + "'";
+			}
+			arr.push(key + "=" + value);
+		}
+	}
+	return arr.toString();
+}
 
 var orm = {
 	selectAll: function(tableName, cb){
 		var query = "SELECT * FROM " + tableName + ";";
 
-		connection.query(queryString, function(err, res){
+		connection.query(query, function(err, res){
 			if(err) throw err;
 			cb(res);
 		});
@@ -12,11 +38,12 @@ var orm = {
 
 	insertOne: function(tableName, colName, value, cb){
 		var query = "INSERT INTO " + tableName;
-		query += " (" + colName + ") ";
-		query += "VALUES " + "(" + value + ")" + ";";
-
-		// debugger
-		console.log(query);
+		query += " (";
+		query += colName.toString();
+		query += ") ";
+		query += "VALUES (";
+		query += printQuestionMarks(value.length); 
+		query += ") ";
 
 		connection.query(query, function(err, res){
 			if(err) throw err;
@@ -26,8 +53,10 @@ var orm = {
 
 	updateOne: function(tableName, updateVal, condition, cb){
 		var query = "UPDATE " + tableName;
-		query += " SET " + updateVal;
-		query += " WHERE " + condition + ";";
+		query += " SET ";
+		query += obToSql(updateVal);
+		query += " WHERE "; 
+		query += condition;
 
 		connection.query(query, function(err, res){
 			if(err) throw err;
@@ -35,4 +64,5 @@ var orm = {
 		});
 	}
 };
+
 module.exports = orm;
