0a20d792002516c1a60b5705b8708b84be9d294b
Yvette <Yvette@macs-MacBook-Air.local>
2018-03-01 18:51:02 -0800

    created functions for controller

---
+++ model/burger.js
@@ -0,0 +1,21 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+	selectAll: function(cb){
+		orm.selectAll("burgers", function(res){
+			cb(res);
+		});
+	},
+	insertOne: function(colName, value, cb){
+		orm.insertOne("burgers", colName, value, function(res){
+			cb(res);
+		});
+	},
+	updateOne: function(updateVal, condition, cb){
+		orm.updateOne("burgers", updateVal, condition, function(res){
+			cb(res);
+		});
+	}
+};
+
+module.exports = burger;
