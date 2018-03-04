76654712b36b67130938574993e8b9fcc0941b64
Yvette <Yvette@macs-MacBook-Air.local>
2018-03-03 18:43:30 -0800

    added connected to JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-	port: 3306,
-	host: "localhost",
-	user: "root",
-	password: "",
-	database: "burgers_db"
-});
-
-connection.connect(function(err){
-	if(err){
-		console.error("Error occurred: " + err.stack);
-		return;
-	}
-	console.log("Connected as ID " + connection.threadId);
-});
+var connection;
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+        root: 3000,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db",
+    });
+};
 
 // Export for orm.js
 module.exports = connection;
